54dce94afca22db547d5b22870277c8d6b9f2bea
Andrew Glach <andrew.glach@gmail.com>
2022-02-16 07:32:50 -0700

    improved numbers

--- script.js
+++ script.js
@@ -1,42 +1,17 @@
-const one = document.querySelector('#one');
-one.addEventListener('click', () => {
-    firstNumber.push(01);
+const numbers = document.querySelectorAll('.number');
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+numbers.forEach(number => {
+    number.addEventListener('click', e => {
+        if (operator === "") {
+            firstNumber += e.target.innerText;
+            console.log(firstNumber);
+        } else {
+            secondNumber += e.target.innerText;
+            console.log(secondNumber);
+        }
+    })
 })
-
-const two = document.querySelector('#two');
-two.addEventListener('click', () => {
-    firstNumber.push(2);
-})
-
-const three = document.querySelector('#three');
-
-const plus = document.querySelector('#plus');
-
-const four = document.querySelector('#four');
-
-const five = document.querySelector('#five');
-
-const six = document.querySelector('#six');
-
-const minus = document.querySelector('#minus');
-
-const seven = document.querySelector('#seven');
-
-const eight = document.querySelector('#eight');
-
-const nine = document.querySelector('#nine');
-
-const divide = document.querySelector('#divide');
-
-const zero = document.querySelector('#zero');
-
-const clear = document.querySelector('#clear');
-
-const equals = document.querySelector('#equals');
-equals.addEventListener('click', () => {
-    console.log(firstNumber.join(''))
-})
-
-const multiply = document.querySelector('#multiply');
-
-let firstNumber = [];
